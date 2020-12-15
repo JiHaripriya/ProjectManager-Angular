2fae169ecd0b0e10de51fd854b3d9048c0a30159
Haripriya <haripriya@qburst.com>
2020-12-15 13:56:05 +0530

    Customize slider appearance

--- src/app/shared/project-form/project-form.component.ts
+++ src/app/shared/project-form/project-form.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectFormComponent implements OnInit {
 
-  slider: string;
+  slider: string = '0';
   
   constructor() { 
     
